a74c77c5484488588e6862b2dbb2196753049aac
epode4 <epode4@gmail.com>
2024-01-29 13:32:49 +0900

    오차 가정 확인 함수 정의

--- blood_donation_predict.R
+++ blood_donation_predict.R
@@ -21,3 +21,24 @@ train_b1 |> autoplot()
 train_b2 <- train_b |> BoxCox(lambda = 0)
 train_b2 |> autoplot()
 
+
+four <- function(train_a,a=6,...){
+  res <- vector("numeric",a)
+  Time <- time(train_a)
+  for (i in seq(res)){
+    xreg <- cbind(Time,fourier(train_a,K=i))
+    fit <- auto.arima(train_a,xreg=xreg,...)
+    res[i] <- fit$aicc
+  }
+  b <- list(res,kmin <- which.min(res))
+  return(b)
+}
+
+test <- function(train_a,fita,a){
+  res_a <- residuals(fita)
+  df_a <- length(fita$par)
+  lag_a <- min(a,round(length(train_a)/5))
+  ggtsdisplay(res_a,plot.type = "histogram")
+  Box.test(res_a,fitdf=df_a,lag=lag_a,type="Ljung-Box")
+}
+
